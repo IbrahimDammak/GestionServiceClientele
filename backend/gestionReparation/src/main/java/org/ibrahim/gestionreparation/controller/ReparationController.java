123f6def6d95de1402bb463644340accecce84b9
IbrahimDammak <brobarhomdamak@gmail.com>
2024-12-02 19:45:16 +0100

    all modifications

---
+++ backend/gestionReparation/src/main/java/org/ibrahim/gestionreparation/controller/ReparationController.java
@@ -0,0 +1,4 @@
+package org.ibrahim.gestionreparation.controller;
+
+public class ReparationController {
+}
---
+++ backend/gestionReparation/src/main/java/org/ibrahim/gestionreparation/service/ClientService.java
@@ -0,0 +1,4 @@
+package org.ibrahim.gestionreparation.service;
+
+public class ClientService {
+}
