3490bcf4aef4418501e50c6bf4461e6c58bfd929
IbrahimDammak <brobarhomdamak@gmail.com>
2024-12-02 20:33:57 +0100

    backend first commit

---
+++ backend/gestionReparation/src/main/java/org/ibrahim/gestionreparation/model/Appareil.java
@@ -0,0 +1,4 @@
+package org.ibrahim.gestionreparation.model;
+
+public class Appareil {
+}
---
+++ backend/gestionReparation/src/main/java/org/ibrahim/gestionreparation/repository/ApparielRepository.java
@@ -0,0 +1,4 @@
+package org.ibrahim.gestionreparation.repository;
+
+public interface ApparielRepository {
+}
